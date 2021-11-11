b85edb686c900c05256aca3d4564687f38d688c9
TANDRI <thomasandries30071998@gmail.com>
2021-11-11 01:36:42 +0100

    update: domino_snake has to be a linked list

--- Dominoes/task/dominoes/dominoes.py
+++ Dominoes/task/dominoes/dominoes.py
@@ -24,19 +24,30 @@ def picking_dominoes(dominoes):
 
 # check who has the highest domino
 def highest_domino(player_stack, computer_stack, stock_pieces):
-    domino_snake = [0, 0]
+    domino_snake = []
+    highest = [0, 0]
+    found = False
     for domino_counter in range(7):
-        if player_stack[domino_counter][0] == player_stack[domino_counter][1] and player_stack[domino_counter][0] > domino_snake[0]:
-            domino_snake = [player_stack[domino_counter][0], player_stack[domino_counter][1]]
-            index = domino_counter
-        elif computer_stack[domino_counter][0] == computer_stack[domino_counter][1] and computer_stack[domino_counter][0] > domino_snake[0]:
-            domino_snake = [computer_stack[domino_counter][0], computer_stack[domino_counter][1]]
-            index = domino_counter
-    if domino_snake in player_stack:
+        if player_stack[domino_counter][0] == player_stack[domino_counter][1]:
+            domino_snake.append([player_stack[domino_counter][0], player_stack[domino_counter][1]])
+        elif computer_stack[domino_counter][0] == computer_stack[domino_counter][1]:
+            domino_snake.append([computer_stack[domino_counter][0], computer_stack[domino_counter][1]])
+    while found == False:
+        if domino_snake[0][0] < domino_snake[1][0]:
+            domino_snake.pop(0)
+            if len(domino_snake) == 1:
+                found = True
+        elif domino_snake[0][0] > domino_snake[1][0]:
+            domino_snake.pop(1)
+            if len(domino_snake) == 1:
+                found = True
+    if domino_snake[0] in player_stack:
         pair = "player"
+        index = player_stack.index(domino_snake[0])
         player_stack.pop(index)
-    elif domino_snake in computer_stack:
+    elif domino_snake[0] in computer_stack:
         pair = "computer"
+        index = computer_stack.index(domino_snake[0])
         computer_stack.pop(index)
     else:
         dominoes = stock_pieces + player_stack + computer_stack
